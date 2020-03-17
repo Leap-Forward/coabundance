d6fcb6f69ff6ae6f758e9f24bad48cccf05933d8
David Hersey <david@leapforward.us>
2020-03-16 17:39:34 -0700

    showing user location with search input

---
+++ containers/MapContainer.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { View } from 'react-native';
+import MapInput from '../components/MapInput';
+import MyMapView from '../components/MapView';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+class MapContainer extends React.Component {
+    state = {
+        region: {}
+    };
+
+    constructor(props) {
+      super(props);
+    }
+
+    _getLocationAsync = async () => {
+      let { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status !== 'granted') {
+        return {errorMessage: 'Permission to access location was denied'};
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      return location;
+    }    
+
+    componentDidMount() {
+        this.getInitialState();
+    }
+
+    getInitialState() {
+      this._getLocationAsync().then(
+            (data) => {
+                console.log(data);
+                this.setState({
+                    region: {
+                        latitude: data.coords.latitude,
+                        longitude: data.coords.longitude,
+                        latitudeDelta: 0.003,
+                        longitudeDelta: 0.003
+                    }
+                });
+            }
+        );
+    }
+
+    getCoordsFromName(loc) {
+        this.setState({
+            region: {
+                latitude: loc.lat,
+                longitude: loc.lng,
+                latitudeDelta: 0.003,
+                longitudeDelta: 0.003
+            }
+        });
+    }
+
+    onMapRegionChange(region) {
+        this.setState({ region });
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <View style={{ flex: 1 }}>
+                    <MapInput notifyChange={(loc) => this.getCoordsFromName(loc)}
+                    />
+                </View>
+
+                {
+                    this.state.region['latitude'] ?
+                        <View style={{ flex: 1 }}>
+                            <MyMapView
+                                region={this.state.region}
+                                onRegionChange={(reg) => this.onMapRegionChange(reg)} />
+                        </View> : null}
+            </View>
+        );
+    }
+}
+
+export default MapContainer;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, Dimensions } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import * as WebBrowser from 'expo-web-browser';
-import MapView from 'react-native-maps';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
-
-import { MonoText } from '../components/StyledText';
+import { View, StyleSheet } from 'react-native';
+import MapContainer from '../containers/MapContainer';
+import Constants from 'expo-constants';
 
 export default class HomeScreen extends Component {
   state = {
@@ -16,42 +11,13 @@ export default class HomeScreen extends Component {
 
   constructor(props) {
     super(props);
-    this._getLocationAsync();
   }
-  _getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      this.setState({
-        errorMessage: 'Permission to access location was denied',
-      });
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ location });
-  }
+ 
 
   render() {
-    let text = 'Waiting..';
-    if (this.state.errorMessage) {
-      text = this.state.errorMessage;
-    } else if (this.state.location) {
-      text = JSON.stringify(this.state.location);
-    }
-
-
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-
-           <MapView style={styles.mapStyle} initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          }} />
-
-        </ScrollView>
-
+        <MapContainer />
       </View>
     );
   }
@@ -61,128 +27,11 @@ HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  mapStyle: {
-    width: Dimensions.get('window').width ,
-    height: Dimensions.get('window').height * 0.65,
-  },  
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight
+  }
 });
